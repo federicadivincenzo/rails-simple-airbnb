2dd5a982f64b83f3e2e42399559f91ee6bde05d2
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-07-22 01:24:23 +0200

    Flat show controller and style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Clearing database 🧹'
+Flat.delete_all
+puts 'Database cleaned ✨'
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Commerce.product_name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(75..350),
+    number_of_guests: rand(1..5)
+  )
+  puts "Flat #{flat.id} created ✅ "
+end
